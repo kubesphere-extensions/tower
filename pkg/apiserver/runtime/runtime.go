d3f609c5836e169714d41bf20963e92c64d65a59
Xinzhao Xu <z2d@jifangcheng.com>
2023-03-16 17:47:03 +0800

    update api path

--- pkg/apiserver/runtime/runtime.go
+++ pkg/apiserver/runtime/runtime.go
@@ -8,6 +8,6 @@ import (
 
 func NewWebService(version string) *restful.WebService {
 	webservice := new(restful.WebService)
-	webservice.Path(fmt.Sprintf("/apis/%s", version)).Produces(restful.MIME_JSON)
+	webservice.Path(fmt.Sprintf("/kapis/tower.kubesphere.io/%s", version)).Produces(restful.MIME_JSON)
 	return webservice
 }
